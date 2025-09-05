7871e5e2d75df2c5b0aea552543b117e0a7e6766
guilherm-hsbe <guilherm3hsb@gmail.com>
2025-09-04 21:43:26 -0300

    [add] layout compartilhado ejs

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
 const app = express();
 const PORT = 3000;
 
+// EJS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 
+// Diretório público
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.render('index', { title: 'Diário de Sintomas' });
+    res.render('pages/index', { title: 'Início' });
 });
 
 app.listen(PORT, () => {
